6eff298eab17f5af75a2d6368d23fe09037f9c36
mostmark <mostmark>
2021-05-06 09:39:31 +0200

    Added MP Config

--- src/main/java/com/test/HelloResource.java
+++ src/main/java/com/test/HelloResource.java
@@ -7,12 +7,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class HelloResource {
+
+    @ConfigProperty(name = "environment", defaultValue="local")
+    String environment;
+    @ConfigProperty(name = "name", defaultValue="me")
+    String name;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello there! The time is " + new Date();
+        return "Hello " + name + " in " + environment;
     }
 }
